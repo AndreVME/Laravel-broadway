9cf4efbcf2676aee52b6f86525fa961364a5551c
Pablo Maria Martelletti <pablo.martelletti@vmeretail.com>
2018-09-10 17:05:39 +0200

    Add aggregate type field migration for ES.

---
+++ migrations/2018_09_10_170811_add_event_store_aggregate_type_field.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventStoreTable extends Migration
+{
+    /**
+     * @var string
+     */
+    private $eventStoreTableName;
+
+    public function __construct()
+    {
+        $this->eventStoreTableName = Config::get('broadway.event-store-table', 'event_store');
+    }
+
+    public function up()
+    {
+        Schema::table($this->eventStoreTableName, function (Blueprint $table) {
+            $table->text('aggregate_type');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table($this->eventStoreTableName, function (Blueprint $table) {
+            $table->dropColumn(['aggregate_type']);
+        });
+    }
+}
